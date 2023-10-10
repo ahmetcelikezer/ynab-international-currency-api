42b9df9454a3b71ee2ee6da1f5db779703d7c7ae
Ahmet Celikezer <ahmet@celikezer.com>
2023-10-11 02:00:56 +0300

    feat(framework): configure linters

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
+  const PORT = 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
-bootstrap();
+bootstrap().catch((err) => console.error(err));
