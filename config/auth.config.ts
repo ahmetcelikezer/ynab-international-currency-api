833138c222c255b7a1969dd84af55a6a33fd4699
Ahmet Celikezer <ahmet@celikezer.com>
2023-12-05 01:15:35 +0300

    imp(config): add required jwt configurations

--- config/auth.config.ts
+++ config/auth.config.ts
@@ -6,6 +6,9 @@ export const AUTH_CONFIG_TOKEN = Symbol('config.auth');
 export type TAuthConfig = {
   secretKey: string;
   saltRounds: number;
+  jwtIssuer: string;
+  jwtAudience: string;
+  jwtExpirationTime: string;
 };
 
 export default registerAs(AUTH_CONFIG_TOKEN.toString(), (): TAuthConfig => {
@@ -14,6 +17,11 @@ export default registerAs(AUTH_CONFIG_TOKEN.toString(), (): TAuthConfig => {
     saltRounds: parseInt(
       Config.getEnvironmentVariable('AUTH_PASSWORD_SALT_ROUNDS'),
       10,
+    ),
+    jwtIssuer: Config.getEnvironmentVariable('AUTH_JWT_ISSUER'),
+    jwtAudience: Config.getEnvironmentVariable('AUTH_JWT_AUDIENCE'),
+    jwtExpirationTime: Config.getEnvironmentVariable(
+      'AUTH_JWT_EXPIRATION_TIME_SECOND',
     ),
   };
 });
